92b04991c87d125ad722f40e98d9d9f6ecd8fdf7
Shumei Huang <rose1264@gmail.com>
2018-06-08 18:55:17 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,11 +4,11 @@
 def speak_to_grandma(words)
 
   if words == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
+    "I LOVE YOU TOO PUMPKIN!"
   elsif words == words.upcase
-    puts "NO, NOT SINCE 1938!"
+    "NO, NOT SINCE 1938!"
   else
-    puts "HUH?! SPEAK UP, SONNY!"
+    "HUH?! SPEAK UP, SONNY!"
   end
 
 end
